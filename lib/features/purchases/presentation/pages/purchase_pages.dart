0527365f753d7fd1de31ddbf2b02ef44d30148d8
Barsha Priyadarshini Das <dasbarsha0014@gmail.com>
2022-12-15 09:06:31 +0530

    folder structure added

---
+++ lib/features/purchases/presentation/pages/purchase_pages.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class PurchasePage extends StatefulWidget {
+  const PurchasePage({super.key});
+
+  @override
+  State<PurchasePage> createState() => _PurchasePageState();
+}
+
+class _PurchasePageState extends State<PurchasePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
