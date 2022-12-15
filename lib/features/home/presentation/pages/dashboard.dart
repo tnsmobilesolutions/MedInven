d3c76a34d48eed0c354062cf645fae623c016020
Barsha Priyadarshini Das <dasbarsha0014@gmail.com>
2022-12-15 08:55:56 +0530

    folder added

---
+++ lib/features/home/presentation/pages/dashboard.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Dashboard extends StatefulWidget {
+  const Dashboard({super.key});
+
+  @override
+  State<Dashboard> createState() => _DashboardState();
+}
+
+class _DashboardState extends State<Dashboard> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/features/inventory/presentation/pages/inventorypage.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class Inventory extends StatefulWidget {
+  const Inventory({super.key});
+
+  @override
+  State<Inventory> createState() => _InventoryState();
+}
+
+class _InventoryState extends State<Inventory> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
